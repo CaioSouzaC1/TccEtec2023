7f1bddd9bf76401733fb96a0507d310564486843
Caio César de Souza <108900609+CaioSouzaC1@users.noreply.github.com>
2023-06-04 12:18:28 -0300

    fix copy profile link

--- Front-End/src/Utils/Admin/index.jsx
+++ Front-End/src/Utils/Admin/index.jsx
@@ -1,9 +1,9 @@
 export const eventTypes = ["Show - Artista", "Show - Banda", "Stand Up"];
 
 // Prod
-// export const API_URL = "https://voice.api.caiosouza.dev/";
-// export const APP_URL = "https://voice.caiosouza.dev/";
+export const API_URL = "https://voice.api.caiosouza.dev/";
+export const APP_URL = "https://voice.caiosouza.dev/";
 
 //Dev
-export const API_URL = "http://localhost:3333/";
-export const APP_URL = "http://localhost:5173/";
+// export const API_URL = "http://localhost:3333/";
+// export const APP_URL = "http://localhost:5173/";
